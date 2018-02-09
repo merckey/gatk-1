39c77e8b9fe4f7f2c5c125f3d7f277debc45cd75
tedsharpe <tsharpe@broadinstitute.org>
2018-02-09 14:33:34 -0500

    wip

--- src/main/java/org/broadinstitute/hellbender/tools/spark/sv/discovery/prototype/KmerAdjacencyBuilder.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/sv/discovery/prototype/KmerAdjacencyBuilder.java
@@ -1,6 +1,5 @@
 package org.broadinstitute.hellbender.tools.spark.sv.discovery.prototype;
 
-import htsjdk.samtools.util.SequenceUtil;
 import org.broadinstitute.barclay.argparser.Argument;
 import org.broadinstitute.barclay.argparser.BetaFeature;
 import org.broadinstitute.barclay.argparser.CommandLineProgramProperties;
@@ -13,7 +12,11 @@ import org.broadinstitute.hellbender.tools.spark.sv.utils.SVKmer.Base;
 import org.broadinstitute.hellbender.tools.spark.sv.utils.SVFastqUtils.FastqRead;
 import scala.Tuple2;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
+import java.util.stream.IntStream;
 
 @BetaFeature
 @CommandLineProgramProperties(
@@ -25,25 +28,34 @@ public class KmerAdjacencyBuilder extends CommandLineProgram {
     @Argument(doc = "input fastq",
             shortName = StandardArgumentDefinitions.INPUT_SHORT_NAME,
             fullName = StandardArgumentDefinitions.INPUT_LONG_NAME)
-    public String fastqFile;
+    private String fastqFile;
+
+    @Argument(doc = "graph output",
+            shortName = StandardArgumentDefinitions.OUTPUT_SHORT_NAME,
+            fullName = StandardArgumentDefinitions.OUTPUT_LONG_NAME)
+    private String graphOutput;
+
+    @Argument(doc = "kmer size", fullName = "kSize", optional = true)
+    private int kSizeArg = 63;
+
+    @Argument(doc = "minimum quality", fullName = "minQ", optional = true)
+    private int minQArg = 10;
+
+    @Argument(doc = "minimum kmer count", fullName = "minKCount", optional = true)
+    private int minKCountArg = 4;
 
     @Override protected Object doWork() {
         final List<FastqRead> reads = SVFastqUtils.readFastqFile(fastqFile);
-        final int kSize = 63;
-        final int minQ = 10;
-        final int minKCount = 4;
+        final int kSize = kSizeArg;
+        final int minQ = minQArg;
+        final int minKCount = minKCountArg;
 
         // kmerize each read, counting the observations of each kmer.
         // trim each read at the first base having a quality less than minQ
         final int nKmers = reads.stream().mapToInt(read -> Math.min(0, read.getBases().length - kSize + 1)).sum();
         final Map<SVKmerLong, Integer> kmerCounts = new HashMap<>(SVUtils.hashMapCapacity(nKmers));
         for ( final FastqRead read : reads ) {
-            final byte[] quals = read.getQuals();
-            int trimLen;
-            for ( trimLen = 0; trimLen != quals.length; ++trimLen ) {
-                if ( quals[trimLen] < minQ ) break;
-            }
-            SVKmerizer.canonicalStream(Arrays.copyOf(read.getBases(),trimLen),kSize,new SVKmerLong(kSize))
+            SVKmerizer.canonicalStream(trimmedRead(read, minQ), kSize, new SVKmerLong(kSize))
                     .forEach(kmer -> kmerCounts.merge((SVKmerLong)kmer, 1, Integer::sum));
         }
 
@@ -129,17 +141,18 @@ public class KmerAdjacencyBuilder extends CommandLineProgram {
         // build contigs
         final List<Contig> contigs = new ArrayList<>();
         final Map<SVKmerLong, ContigEnd> contigEnds = new HashMap<>();
+        final Map<SVKmerLong, ContigLocus> locusMap = new HashMap<>(SVUtils.hashMapCapacity(kmerAdjacencyMap.size()));
         kmerAdjacencyMap.forEach( (kmer, counts) -> {
             if ( !contigEnds.containsKey(kmer) ) {
                 final int contigId = contigs.size();
                 Contig contig = null;
                 final SVKmerLong predecessor = getSolePredecessor(kmer, kSize2, counts);
                 if ( predecessor == null || getSuccessorCount(predecessor, kSize2, kmerAdjacencyMap) > 1 ) {
-                    contig = buildContig(kmer, kSize2, counts, kmerAdjacencyMap, contigId);
+                    contig = buildContig(kmer, kSize2, counts, kmerAdjacencyMap, contigId, locusMap);
                 } else {
                     final SVKmerLong successor = getSoleSuccessor(kmer, kSize2, counts);
                     if ( successor == null || getPredecessorCount(successor, kSize2, kmerAdjacencyMap) > 1 ) {
-                        contig = buildContig(kmer.reverseComplement(kSize2), kSize2, counts, kmerAdjacencyMap, contigId);
+                        contig = buildContig(kmer.reverseComplement(kSize2), kSize2, counts, kmerAdjacencyMap, contigId, locusMap);
                     }
                 }
                 if ( contig != null ) {
@@ -151,47 +164,103 @@ public class KmerAdjacencyBuilder extends CommandLineProgram {
         });
 
         // dump contigs
-        for ( final Contig contig : contigs ) {
-            final int contigId = contig.getFirst().getContigId();
-            final int seqLen = contig.getSequence().length();
-            System.out.println("S\t" + contigId + "\t" + contig.getSequence() + "\tLN:i:" + seqLen);
-            for ( final SVKmerLong kmer : contig.getFirst().getConnections() ) {
-                final ContigEnd target = contigEnds.get(kmer);
-                if ( target.getContigId() >= contigId ) {
-                    final String dir = target.isFirst() ? "+" : "-";
-                    System.out.println("L\ttig" + contigId + "\t-\t" + target.getContigId() + "\t" + dir);
-                    System.out.println(SequenceUtil.reverseComplement(contig.getSequence().substring(0, 60)));
-                    final String targetSequence = contigs.get(target.getContigId()).getSequence();
-                    final String targetSubSequence;
-                    if ( target.isFirst() ) {
-                        targetSubSequence = targetSequence.substring(0, 60);
-                    } else {
-                        targetSubSequence =
-                                SequenceUtil.reverseComplement(targetSequence.substring(targetSequence.length()-60));
-                    }
-                    System.out.println(targetSubSequence);
+        dumpGFA( contigs, contigEnds, graphOutput + ".gfa" );
+        dumpFASTA( contigs, graphOutput + ".fasta" );
+        dumpDOT( contigs, contigEnds, graphOutput + ".dot" );
+/*
+        reads.forEach(read -> {
+            System.out.println(read.getHeader());
+            SVKmerizer.stream(trimmedRead(read,minQ), kSize2, new SVKmerLong()).forEach(kmer -> {
+                final ContigLocus locus = locusMap.get(((SVKmerLong)kmer).canonical(kSize2));
+                if ( locus == null ) {
+                    System.out.println("  no mapping");
+                } else if ( ((SVKmerLong)kmer).isCanonical(kSize2) == locus.isCanonical() ) {
+                    System.out.println("  " + locus.getContigId() + ":" + locus.getOffset());
+                } else {
+                    System.out.println("  " + locus.getContigId() + ":" + ~locus.getOffset());
                 }
-            }
-            for ( final SVKmerLong kmer : contig.getLast().getConnections() ) {
-                final ContigEnd target = contigEnds.get(kmer);
-                if ( target.getContigId() >= contigId ) {
-                    final String dir = target.isFirst() ? "+" : "-";
-                    System.out.println("L\ttig" + contigId + "\t+\t" + target.getContigId() + "\t" + dir);
-                    System.out.println(contig.getSequence().substring(contig.getSequence().length()-60));
-                    final String targetSequence = contigs.get(target.getContigId()).getSequence();
-                    final String targetSubSequence;
-                    if ( target.isFirst() ) {
-                        targetSubSequence = targetSequence.substring(0, 60);
-                    } else {
-                        targetSubSequence =
-                                SequenceUtil.reverseComplement(targetSequence.substring(targetSequence.length()-60));
-                    }
-                    System.out.println(targetSubSequence);
-                }
-            }
-        }
-
+            });
+        });
+*/
         return null;
+    }
+
+    private byte[] trimmedRead( final FastqRead read, final int minQ ) {
+        final byte[] quals = read.getQuals();
+        final int trimLen =
+                IntStream.range(0, quals.length).filter(idx -> quals[idx] < minQ).findFirst().orElse(quals.length);
+        return Arrays.copyOf(read.getBases(), trimLen);
+    }
+
+    private void dumpDOT( final List<Contig> contigs, final Map<SVKmerLong, ContigEnd> contigEnds, final String fileName ) {
+        try ( final BufferedWriter writer = new BufferedWriter(new FileWriter(fileName)) ) {
+            writer.write("digraph {\n");
+            for ( final Contig contig : contigs ) {
+                final int id = contig.getFirst().getContigId();
+                final double width = contig.getSequence().length() / 100.;
+                writer.write("tig" + id + " [width=" + width + "]\n");
+                writer.write("tig" + id + "RC [width=" + width + "]\n");
+            }
+            for ( final Contig contig : contigs ) {
+                final int id = contig.getFirst().getContigId();
+                for ( final Connection connection : contig.getFirst().getConnections() ) {
+                    final ContigEnd target = contigEnds.get(connection.getKmer());
+                    if ( target.getContigId() >= id ) {
+                        final String label = " [label=" + connection.getWeight() + "]\n";
+                        writer.write("tig" + id + "RC -> tig" + target.getContigId() + (target.isFirst() ? "" : "RC") + label);
+                        writer.write("tig" + target.getContigId() + (target.isFirst() ? "RC" : "") + " -> tig" + id + label);
+                    }
+                }
+                for ( final Connection connection : contig.getLast().getConnections() ) {
+                    final ContigEnd target = contigEnds.get(connection.getKmer());
+                    if ( target.getContigId() >= id ) {
+                        final String label = " [label=" + connection.getWeight() + "]\n";
+                        writer.write("tig" + id + " -> tig" + target.getContigId() + (target.isFirst() ? "" : "RC") + label);
+                        writer.write("tig" + target.getContigId() + (target.isFirst() ? "RC" : "") + " -> tig" + id + "RC" + label);
+                    }
+                }
+            }
+            writer.write("}\n");
+        } catch ( final IOException ioe ) {
+            throw new GATKException("Failed to write assembly DOT file.", ioe);
+        }
+    }
+
+    private void dumpFASTA( final List<Contig> contigs, final String fileName ) {
+        try ( final BufferedWriter writer = new BufferedWriter(new FileWriter(fileName)) ) {
+            for ( final Contig contig : contigs ) {
+                writer.write(">" + contig.getFirst().getContigId() + "\n");
+                writer.write(contig.getSequence() + "\n");
+            }
+        } catch ( final IOException ioe ) {
+            throw new GATKException("Failed to write assembly FASTA file.", ioe);
+        }
+    }
+
+    private void dumpGFA( final List<Contig> contigs, final Map<SVKmerLong, ContigEnd> contigEnds, final String fileName ) {
+        try ( final BufferedWriter writer = new BufferedWriter(new FileWriter(fileName)) ) {
+            for ( final Contig contig : contigs ) {
+                final int contigId = contig.getFirst().getContigId();
+                final int seqLen = contig.getSequence().length();
+                writer.write("S\t" + contigId + "\t" + contig.getSequence() + "\tLN:i:" + seqLen + "\n");
+                for ( final Connection connection : contig.getFirst().getConnections() ) {
+                    final ContigEnd target = contigEnds.get(connection.getKmer());
+                    if ( target.getContigId() >= contigId ) {
+                        final String dir = target.isFirst() ? "+" : "-";
+                        writer.write("L\ttig" + contigId + "\t-\ttig" + target.getContigId() + "\t" + dir + "\n");
+                    }
+                }
+                for ( final Connection connection : contig.getLast().getConnections() ) {
+                    final ContigEnd target = contigEnds.get(connection.getKmer());
+                    if ( target.getContigId() >= contigId ) {
+                        final String dir = target.isFirst() ? "+" : "-";
+                        writer.write("L\ttig" + contigId + "\t+\ttig" + target.getContigId() + "\t" + dir + "\n");
+                    }
+                }
+            }
+        } catch ( final IOException ioe ) {
+            throw new GATKException("Failed to write assembly FASTA file.", ioe);
+        }
     }
 
     private static SVKmerLong getSolePredecessor( final SVKmerLong kmer, final int kSize, final int[] counts ) {
@@ -247,17 +316,21 @@ public class KmerAdjacencyBuilder extends CommandLineProgram {
     }
 
     private static Contig buildContig( final SVKmerLong kmerArg, final int kSize, final int[] countsArg,
-                                     final Map<SVKmerLong, int[]> kmerAdjacencyMap, final int contigId ) {
+                                       final Map<SVKmerLong, int[]> kmerAdjacencyMap, final int contigId,
+                                       final Map<SVKmerLong, ContigLocus> locusMap ) {
         final ContigEnd firstEnd = new ContigEnd(contigId, true, kmerArg, kSize, countsArg);
         final StringBuilder sb = new StringBuilder(kmerArg.toString(kSize));
         SVKmerLong kmer = kmerArg;
         SVKmerLong lastKmer = kmerArg;
         int[] counts = countsArg;
         int[] lastCounts = countsArg;
+        int offset = 0;
+        locusMap.put(kmer.canonical(kSize), new ContigLocus(contigId, offset++, kmer.isCanonical(kSize)));
         while ( (kmer = getSoleSuccessor(kmer, kSize, counts)) != null ) {
             counts = kmerAdjacencyMap.get(kmer.canonical(kSize));
             if ( (kmer.isCanonical(kSize) ? getPredecessorCount(counts) : getSuccessorCount(counts)) != 1 ) break;
             sb.append(kmer.lastBase().toString());
+            locusMap.put(kmer.canonical(kSize), new ContigLocus(contigId, offset++, kmer.isCanonical(kSize)));
             lastKmer = kmer;
             lastCounts = counts;
         }
@@ -265,12 +338,41 @@ public class KmerAdjacencyBuilder extends CommandLineProgram {
         return new Contig(sb.toString(), firstEnd, lastEnd);
     }
 
+    private static final class ContigLocus {
+        private final int contigId;
+        private final int offset;
+        private final boolean canonical;
+
+        public ContigLocus( final int contigId, final int offset, final boolean canonical ) {
+            this.contigId = contigId;
+            this.offset = offset;
+            this.canonical = canonical;
+        }
+
+        public int getContigId() { return contigId; }
+        public int getOffset() { return offset; }
+        public boolean isCanonical() { return canonical; }
+    }
+
+    private static final class Connection {
+        private final SVKmerLong kmer;
+        private final int weight;
+
+        public Connection( final SVKmerLong kmer, final int weight ) {
+            this.kmer = kmer;
+            this.weight = weight;
+        }
+
+        public SVKmerLong getKmer() { return kmer; }
+        public int getWeight() { return weight; }
+    }
+
     private static final class ContigEnd {
         private final SVKmerLong kmer;
         private final int contigId;
         private final boolean isFirst;
         private final boolean isCanonical;
-        private final List<SVKmerLong> connections;
+        private final List<Connection> connections;
 
         public ContigEnd( final int contigId, final boolean isFirst,
                           final SVKmerLong kmer, final int kSize, final int[] counts ) {
@@ -287,30 +389,35 @@ public class KmerAdjacencyBuilder extends CommandLineProgram {
         public int getContigId() { return contigId; }
         public boolean isFirst() { return isFirst; }
         public boolean isCanonical() { return isCanonical; }
-        public List<SVKmerLong> getConnections() { return Collections.unmodifiableList(connections); }
+        public List<Connection> getConnections() { return Collections.unmodifiableList(connections); }
 
         private void addConnections( final int kSize, final int[] counts ) {
             for ( final Base base : Base.values() ) {
+                final int weight;
+                SVKmerLong connectingKmer = null;
                 if ( isFirst ) {
                     if ( isCanonical ) {
-                        if ( counts[base.ordinal()] > 0 ) {
-                            connections.add(kmer.predecessor(base, kSize).canonical(kSize));
+                        if ( (weight = counts[base.ordinal()]) > 0 ) {
+                            connectingKmer = kmer.predecessor(base, kSize).canonical(kSize);
                         }
                     } else {
-                        if ( counts[4 + base.complement().ordinal()] > 0 ) {
-                            connections.add(kmer.successor(base.complement(), kSize).canonical(kSize));
+                        if ( (weight = counts[4 + base.complement().ordinal()]) > 0 ) {
+                            connectingKmer = kmer.successor(base.complement(), kSize).canonical(kSize);
                         }
                     }
                 } else {
                     if ( isCanonical ) {
-                        if ( counts[4 + base.ordinal()] > 0 ) {
-                            connections.add(kmer.successor(base, kSize).canonical(kSize));
+                        if ( (weight = counts[4 + base.ordinal()]) > 0 ) {
+                            connectingKmer = kmer.successor(base, kSize).canonical(kSize);
                         }
                     } else {
-                        if ( counts[base.complement().ordinal()] > 0 ) {
-                            connections.add(kmer.predecessor(base.complement(), kSize).canonical(kSize));
+                        if ( (weight = counts[base.complement().ordinal()]) > 0 ) {
+                            connectingKmer = kmer.predecessor(base.complement(), kSize).canonical(kSize);
                         }
                     }
+                }
+                if ( weight > 0 ) {
+                    connections.add(new Connection(connectingKmer, weight));
                 }
             }
         }
